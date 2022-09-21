5a354c7ea9660576a146894c535a64a05f8eea1d
Alexander Fallah <alexander.fallah@therealreal.com>
2022-09-20 21:15:09 -0400

    Initial Commit

---
+++ RocketMortgageUITests/RocketMortgageUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RocketMortgageUITestsLaunchTests.swift
+//  RocketMortgageUITests
+//
+//  Created by Alexander Fallah on 9/20/22.
+//
+
+import XCTest
+
+class RocketMortgageUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
